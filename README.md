e7c5970b0cbf104c7b6783529af7f15abc9bd535
Xeno <xenophilicy@gmail.com>
2020-02-09 02:34:20 -0600

    Update v1.2.0
    Add customizable blocks and moved list mode to onRun()

--- README.md
+++ README.md
@@ -6,14 +6,14 @@
 # [![Xenophilicy](https://i.imgur.com/ItFx1ZH.png)]()
 
 ## Information
-With this plugin you are able to setup ores to spawn in a classic cobblestone generator! Each time a block spawns in a cobblstone generator, it has a chance to change into a certain ore. These chances can be changed in the plugin's *config.yml* file. You can black/whitelist certain worlds for OreGen to function, or have it function in all worlds at once as well!
+With this plugin you are able to set up blocks to spawn in a classic cobblestone generator rather than plain cobblestone blocks! Each time a block spawns in a cobblstone generator, it has a chance to change to a desired block. These blocks and their probability can be changed in the plugin's *config.yml* file. You can black/whitelist certain worlds for OreGen to function in, or have it function in all worlds at once!
 
 *You can find a video example on YouTube → https://www.youtube.com/watch?v=Fb6llO6q9Ps*
 ***
 
 ## OreGen Details
 * **API:** 3.5.0+
-* **Version:** 1.1.4
+* **Version:** 1.2.0
 * **Basic Description:** Generate ores inside a cobble generator!
 * *Easy to edit config.yml file*
 * *Simple code for editing and debugging*
--- src/Xenophilicy/OreGen/OreGen.php
+++ src/Xenophilicy/OreGen/OreGen.php
@@ -24,127 +24,123 @@ use pocketmine\event\Listener;
 class OreGen extends PluginBase implements Listener{
     
     private $config;
-    private $oreList = [];
+    private $listMode;
+    private $blockList = [];
+    private $levels = [];
 
 	public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
         $this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
         $this->config->getAll();
-        $this->getLogger()->info("OreGen has been enabled!");
         $version = $this->config->get("VERSION");
-        if($version != "1.1.4"){
-            $this->getLogger()->warning("You have updated OreGen but are using an old config! Please delete your outdated config for new features to be enabled!");
+        if($version !== "1.2.0"){
+            $this->getLogger()->warning("You have updated OreGen but are using an old config! Please delete your outdated config to continue using OreGen!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+            return;
         }
-        if($this->config->get("World-List") !== null && $this->config->get("World-List") !== []){
-            if($this->config->get("List-Mode") == null || $this->config->get("List-Mode") == ""){
-                $this->getLogger()->error("The list mode cannot be left null! Please choose either 'Blacklist' or 'Whitelist'! Disabling plugin...");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return false;
+        $levels = scandir($this->getServer()->getDataPath()."worlds/");
+        foreach($levels as $level){
+            if($level === "." || $level === ".."){
+                continue;
+            } else{
+                $this->getServer()->loadLevel($level); 
             }
+        }
+        $worldList = $this->config->getNested("Worlds.List");
+        $this->list = $this->config->get("List");
+        foreach ($worldList as $world) {
+            $level = $this->getServer()->getLevelByName($world);
+            if($level === null){
+                $this->getLogger()->critical("Invalid world name! Name: ".$world." was not found, disabling plugin! Be sure you use the name of the world folder for the world name in the config!");
+                $this->getServer()->getPluginManager()->disablePlugin($this);
+                return;
+            } else{
+                array_push($this->levels,$world);
+            }
+        }
+        $mode = strtolower($this->config->getNested("Worlds.Mode"));
+        switch($mode){
+            case "whitelist":
+                $this->listMode = "wl";
+                break;
+            case "blacklist":
+                $this->listMode = "bl";
+            break;
+            case false:
+                $this->listMode = false;
+                break;
+            default:
+                $this->getLogger()->error("Invalid world list mode! Valid modes are 'blacklist', 'whitelist', or false. Invalid mode: ".$mode." is not supported!, disabling plugin...");
+                $this->getServer()->getPluginManager()->disablePlugin($this);
+                return;
         }
         $this->buildProbability();
 	}
 
-    public function buildProbability() : bool{
-        $list = [];
+    public function buildProbability(){
         $cobbleProb = $this->config->get("Cobble-Probability");
         if(!is_numeric($cobbleProb)){
-            $this->getLogger()->error("Cobblestone probability must be numerical! Disabling plugin...");
+            $this->getLogger()->error("Cobblestone probability must be numerical, disabling plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
-            return false;
+            return;
         }
         for($i=0;$i<$cobbleProb;$i++){
-            $this->oreList = array_push($list,'Cobble');
+            array_push($this->blockList,"4:0");
         }
-        $sum = $cobbleProb;
-        $ores = ['Coal','Iron','Gold','Lapis','Redstone','Emerald','Diamond'];
-        foreach($ores as $ore){
-            $enabled = $this->config->getNested($ore.".Enabled");
-            if($enabled === true){
-                $chance = $this->config->getNested("$ore.Probability");
-                if(is_numeric($chance)){
-                    $sum = $sum + $chance;
-                    for($i=0;$i<$chance;$i++){
-                        $this->oreList = array_push($list,$ore);
-                    }
-                }
-                else{
-                    $this->getLogger()->warning("Ore '".$ore."' has an invalid value, it will be disabled!");
-                }
+        $probSum = $cobbleProb;
+        $blocks = $this->config->get("Blocks");
+        foreach($blocks as $block => $probability){
+            $values = explode(":",$block);
+            try{
+                $test = Block::get((int)$values[0],isset($values[1]) ? (int)$values[1]:0);
+            } catch(\InvalidArgumentException $e){
+                $this->getLogger()->warning("Invalid block! Block ".$block." was not found, it will be disabled!");
+                continue;
             }
-            elseif($enabled !== false){
-                $this->getLogger()->warning("Ore '".$ore."' has an invalid value, it will be disabled!");
+            $chance = $this->config->getNested("Blocks.".$block);
+            if(is_numeric($chance)){
+                $probSum += $chance;
+                for($i=0;$i<$chance;$i++){
+                    array_push($this->blockList,$block);
+                }
+            } else{
+                $this->getLogger()->warning("Invalid block probablity! Block ".$block." has an invalid probability, it will be disabled!");
             }
         }
-        $this->oreList = $list;
-        if($sum != 100){
-            $this->getLogger()->error("Ore probability has a sum of ".$sum);
-            $this->getLogger()->error("Probability must have a sum equal to 100! Disabling plugin...");
+        if($probSum != 100){
+            $this->getLogger()->error("Block probability has a sum of ".$probSum.", it must have a sum of 100, disabling plugin...");
             $this->getServer()->getPluginManager()->disablePlugin($this);
-            return false;
+            return;
         }
-        return true;
     }
 
-    public function onBlockSet(BlockUpdateEvent $event){
-        if($this->config->get("World-List") !== false){
-            if($this->config->get("List-Mode") == "Whitelist"){
-                if(!in_array($event->getBlock()->getLevel()->getName(), $this->config->get("World-List"))){
-                    return;
-                }
-            }
-            elseif($this->config->get("List-Mode") == "Blacklist"){
-                if(in_array($event->getBlock()->getLevel()->getName(), $this->config->get("World-List"))){
-                    return;
-                }
-            }
+    public function onBlockUpdate(BlockUpdateEvent $event){
+        $levelName = $event->getBlock()->getLevel()->getName();
+        if(($this->listMode == "wl" && !in_array($levelName,$this->levels)) || ($this->listMode == "bl" && in_array($levelName,$this->levels))){
+            return;
+        } else{
+            $this->blockSet($event);
         }
-        $this->blockSet($event);
     }
 
     public function blockSet($event){
         $block = $event->getBlock();
         $waterPresent = false;
         $lavaPresent = false;
-        if ($block == "Block[Cobblestone] (4:0)"){
+        if ($block->getId() == 4 && $block->getDamage() == 0){
             for ($target = 2; $target <= 5; $target++) {
                 $blockSide = $block->getSide($target);
                 if ($blockSide instanceof Water) {
                     $waterPresent = true;
-                }
-                elseif ($blockSide instanceof Lava) {
+                } elseif ($blockSide instanceof Lava) {
                     $lavaPresent = true;
                 }
                 if ($waterPresent && $lavaPresent) {
-                    $pb = array_rand($this->oreList,1);
-                    switch($this->oreList[$pb]){
-                        case "Coal":
-                            $placeBlock = Block::get(Block::COAL_ORE);
-                            break;
-                        case "Iron":
-                            $placeBlock = Block::get(Block::IRON_ORE);
-                            break;
-                        case "Gold":
-                            $placeBlock = Block::get(Block::GOLD_ORE);
-                            break;
-                        case "Lapis":
-                            $placeBlock = Block::get(Block::LAPIS_ORE);
-                            break;
-                        case "Redstone":
-                            $placeBlock = Block::get(Block::REDSTONE_ORE);
-                            break;
-                        case "Emerald":
-                            $placeBlock = Block::get(Block::EMERALD_ORE);
-                            break;
-                        case "Diamond":
-                            $placeBlock = Block::get(Block::DIAMOND_ORE);
-                            break;
-                        default:
-                            $placeBlock = Block::get(Block::COBBLESTONE);
-                    }
-                    $block->getLevel()->setBlock($block, $placeBlock, false, false);
-                    return true;
+                    $pb = array_rand($this->blockList,1);
+                    $values = explode(":",$this->blockList[$pb]);
+                    $event->setCancelled();
+                    $block->getLevel()->setBlock($block, Block::get($values[0],$values[1]), false, false);
                 }
             }
         }
